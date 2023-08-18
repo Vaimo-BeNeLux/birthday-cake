72e8b80e0e9d9acfb78cbc3a60bcc54fe7cc59ee
Dmytro Makukh <dmytro.makukh@vaimo.com>
2023-08-18 16:53:54 +0300

    added database connection

--- server/firestore.js
+++ server/firestore.js
@@ -1,48 +1,43 @@
 import {
     collection,
     getDocs,
-    getDoc,
     addDoc,
     deleteDoc,
     doc,
-    query,
-    where,
-    setDoc,
-    collectionGroup,
-    Timestamp,
-  } from "firebase/firestore";
-  import { firestoreDb } from "./firebase";
+    setDoc
+  } from "firebase/firestore"
+  import { firestoreDb } from "./firebase"
   
-  export const queryByCollection = async (col) => {
+  export const queryByCollection = async col => {
     // @ts-ignore
-    const colRef = collection(firestoreDb, col);
+    const colRef = collection(firestoreDb, col)
   
-    const snapshot = await getDocs(colRef);
+    const snapshot = await getDocs(colRef)
   
-    const docs = Array.from(snapshot.docs).map((doc) => {
+    const docs = Array.from(snapshot.docs).map(doc => {
       return {
         ...doc.data(),
-        id: doc.id,
-      };
-    });
+        id: doc.id
+      }
+    })
   
-    return docs;
-  };
+    return docs
+  }
   
   export const set = async (col, document) => {
-    await setDoc(doc(collection(firestoreDb, col)), document, { merge: true });
-  };
+    await setDoc(doc(collection(firestoreDb, col)), document, { merge: true })
+  }
   
   export const add = async (col, document) => {
-    // @ts-ignore
-    const colRef = collection(firestoreDb, col);
+    const colRef = collection(firestoreDb, col)
   
-    const docRef = await addDoc(colRef, document);
+    const docRef = await addDoc(colRef, document)
   
-    return docRef;
-  };
+    return docRef
+  }
   
   export const del = async (col, id) => {
-    const docRef = doc(firestoreDb, col, id);
-    return await deleteDoc(docRef);
-  };
+    const docRef = doc(firestoreDb, col, id)
+    return await deleteDoc(docRef)
+  }
+  
