ff1a6f5b2d999819aa4f2c4b25bd1a2cc6e9ce03
Martine Boulanger <martine.boulanger@vaimo.com>
2023-08-18 12:55:12 +0200

    added footer and generatedimages components

--- pages/generator.vue
+++ pages/generator.vue
@@ -1,13 +1,18 @@
 <template>
     <div class="root">
       <Header />
+      <!-- Content Component here -->
+      <GeneratedImages />
+      <Footer />
     </div>
   </template>
   
   <script>
   import Header from '../components/Header/Header.vue';
+  import Footer from '../components/Footer/Footer.vue';
+  import GeneratedImages from '../components/GeneratedImages/GeneratedImages.vue';
   export default {
     name: 'GeneratorPage',
-    components: {Header}
+    components: {Header, Footer, GeneratedImages}
   }
   </script>
